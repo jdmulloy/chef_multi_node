13979afe45dadef18ca48cd41f996326d0698c9c
chef <chef@ip-10-251-68-47.us-west-2.compute.internal>
2015-03-31 17:30:38 +0000

    Added recipes to default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,8 @@
 # Recipe:: default
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
+#
+
+include_recipe 'wordpress-cluster::database'
+include_recipe 'wordpress-cluster::app'
+include_recipe 'wordpress-cluster::lb'
