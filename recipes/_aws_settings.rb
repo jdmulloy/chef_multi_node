ba7c79c7b908260569530625dfae49097a02db8d
chef <chef@ip-10-251-68-47.us-west-2.compute.internal>
2015-03-31 17:59:43 +0000

    Added missing square brackets

--- recipes/_aws_settings.rb
+++ recipes/_aws_settings.rb
@@ -5,11 +5,11 @@ with_driver 'aws'
 with_machine_options(
   bootstrap_options: {
     instance_type: node['wordpress-cluster']['aws']['flavor'],
-    key_name: node['wordpress-cluster'['aws']['key_name'],
-    security_group_ids: node['wordpress-cluster'['aws']['security_group_ids'],
+    key_name: node['wordpress-cluster']['aws']['key_name'],
+    security_group_ids: node['wordpress-cluster']['aws']['security_group_ids'],
   },
   ssh_username: node['wordpress-cluster']['aws']['ssh_username'],
-  image_id: node['wordpress-cluster']['aws']['image_id']
+  image_id:     node['wordpress-cluster']['aws']['image_id']
 )
 
 aws_key_pair node['wordpress-cluster'['aws']['key_name']
