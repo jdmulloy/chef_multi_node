e5b69824153d0bdc137acab8eb7675c8a47094c0
chef <chef@ip-10-251-68-47.us-west-2.compute.internal>
2015-03-31 17:04:04 +0000

    Added attributes file

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['wordpress-cluster']['aws']['flavor'] = 't1.micro'
+default['wordpress-cluster']['aws']['key_name'] = 'josephm354-wp-cluster'
+default['wordpress-cluster']['aws']['security_group_ids'] = 'sg-c49e0ff4'
+default['wordpress-cluster']['aws']['ssh_username'] = 'root'
+default['wordpress-cluster']['aws']['image_id'] = 'ami-37361807'
