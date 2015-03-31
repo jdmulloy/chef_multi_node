4d0ec8940c687fc729c1986a874aabf60bebc044
chef <chef@ip-10-251-68-47.us-west-2.compute.internal>
2015-03-31 19:23:21 +0000

    Fixed typo in lb recipe

--- recipes/lb.rb
+++ recipes/lb.rb
@@ -2,7 +2,7 @@ include_recipe 'wordpress-cluster::_aws_settings'
 
 load_balancer 'josephm354-wp' do
   load_balancer_options :availability_zones => ['us-west-2a', 'us-west-2b', 'us-west-2c'],
-    :listeners +> [{
+    :listeners => [{
       :port => 80,
       :protocol => :http,
       :instance_port => 80,
